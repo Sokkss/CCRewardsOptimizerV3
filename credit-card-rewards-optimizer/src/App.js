52c3b4adcb7ee6baa366f5b264049f30b0ab2d62
Sokkss <124085882+Sokkss@users.noreply.github.com>
2025-02-17 19:30:56 -0500

    trying out authenticator

--- credit-card-rewards-optimizer/src/App.js
+++ credit-card-rewards-optimizer/src/App.js
@@ -1,27 +1,44 @@
 import './App.css';
-
 import HomePage from './components/home/HomePage';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './components/auth/LoginPage';
 import RegisterPage from './components/auth/RegisterPage';
 import SiteNav from './components/common/SiteNav';
 import SiteFooter from './components/common/SiteFooter';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Configure Amplify
+Amplify.configure(awsExports);
+
 function App() {
-  return (
-    <div>
-      <SiteNav />
-      <Routes>
-        <Route path="/" exact={true} element={<HomePage />} />
-        <Route path="*" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-      <SiteFooter />
-    </div>
-  );
+    return (
+        <>
+            <SiteNav />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="*" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route
+                    path="/protected"
+                    element={
+                        <Authenticator>
+                            {({ signOut, user }) => (
+                                <main>
+                                    <h1>Hello {user?.username}</h1>
+                                    <button onClick={signOut}>Sign out</button>
+                                </main>
+                            )}
+                        </Authenticator>
+                    }
+                />
+            </Routes>
+            <SiteFooter />
+        </>
+    );
 }
 
 export default App;
